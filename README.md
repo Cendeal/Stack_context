72a8dbd5adaeb8e08057b0ecb45ef35f2f6576c9
Cendeal <1798062051@qq.com>
2020-06-12 19:32:40 +0800

    reformat

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
 # 关于z-index属性
-
-![image.png](https://cdn.nlark.com/yuque/0/2020/png/1343283/1591855325199-139008e0-ac05-464e-8b96-8e09e6dda95f.png#align=left&display=inline&height=334&margin=%5Bobject%20Object%5D&name=image.png&originHeight=668&originWidth=994&size=580096&status=done&style=none&width=497)
-这张图的总结是存在问题的，是不正确的：
-(1)任意元素的z-index要起作用是由条件的：必须指定position的值(非static)。否则，即使给了z-index属性元素的层叠只与出现顺序有关。**假设图片的条件是正常流(也就是元素没有给position的情况下)**，如果负z-index最后才现，那么它必然是处于最接近用户的顶层，此时图片的结论明显就错了。**再假设它设置了position，使得z-index生效图也是错的**，本身没有设置z-index的情况，默认就是为0层，也就是z-index为0([参考文档](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Positioning/Understanding_z_index/Adding_z-index))。
-![image.png](https://cdn.nlark.com/yuque/0/2020/png/1343283/1591874993096-65ebcaf6-6c06-4ca1-b3f4-7987001acc24.png#align=left&display=inline&height=231&margin=%5Bobject%20Object%5D&name=image.png&originHeight=279&originWidth=900&size=32025&status=done&style=stroke&width=746)
-这种情况其实就是任意元素(无论行内还是块级)本身，它们的层叠顺序就是只和出现顺序有关，最后图片的总结还是错的。这里我写了代码演示可以拷贝去测试，这里是[演示地址](https://cendeal.github.io/Stack_context/)。图中设置z-index的元素position为relative。其中的数字为层级出现顺序。如图:
-![image.png](https://cdn.nlark.com/yuque/0/2020/png/1343283/1591874332016-3d13fdab-a444-4514-bacc-abcd064a7242.png#align=left&display=inline&height=649&margin=%5Bobject%20Object%5D&name=image.png&originHeight=880&originWidth=1011&size=63451&status=done&style=shadow&width=746)
-(2)层叠上下文background/border并没有说明是最底层，还是参考层，如果在图中这样表示应该可以理解为默认层，如果是默认层，那么负z-index的元素应该是置于该默认层之下，参考文档[链接](https://developer.mozilla.org/zh-CN/docs/Web/Guide/CSS/Understanding_z_index/Adding_z-index)。
-![image.png](https://cdn.nlark.com/yuque/0/2020/png/1343283/1591872149979-c7f3e17e-bc97-4f2c-a2a3-ecefb6ba0eca.png#align=left&display=inline&height=432&margin=%5Bobject%20Object%5D&name=image.png&originHeight=466&originWidth=804&size=26172&status=done&style=shadow&width=746)
-(3)按照图片的理解似乎z-index属性与display和float不能共存。
---
+++ src/js/initPage.js
@@ -0,0 +1,235 @@
+//please insert utils.js before
+const is_pc = isPc()
+alert(is_pc)
+if(!is_pc){
+    document.getElementById('pc_change').hidden = true
+}
+let div_boxes = ['zIndexPositive', 'zIndexAuto', 'inline', 'inlineBlock', 'float', 'block', 'zIndexZero', 'zIndexNegative']
+let currentControl = {
+    id: 'current_control',
+    value: '无(点击div选择目标)',
+    position: {
+        top: 0,
+        left: 0
+    }
+}
+bindNode(document.getElementById(currentControl.id), currentControl, 'value')
+const classes = {
+    inline: 'inline inline-color',
+    inlineBlock: 'inline-block inline-block-color',
+    float: 'float float-color',
+    block: 'block block-color',
+    zIndexPositive: 'z-index-positive-color z-index-positive z-index',
+    zIndexZero: 'z-index-zero z-index z-index-zero-color',
+    zIndexNegative: 'z-index-negative z-index z-index-negative-color',
+    zIndexAuto: 'z-index-auto z-index z-index-auto-color'
+}
+
+function bindNode(node, object, property) {
+    Object.defineProperty(object, property, {
+        set(v) {
+            node.innerText = v
+        },
+        get() {
+            return node.innerText
+        }
+    })
+
+}
+
+function generateDiv(item, i, divs) {
+    let temp = document.getElementById(item)
+    let display = ''
+    if (temp) {
+        display = temp.style.display
+        temp.remove()
+    }
+
+    let div = document.createElement('div')
+    div.style.display = display
+    div.innerText = div.title = `#${i}-${item}`
+    div.className = classes[item] + ' box'
+    div.id = item
+    div.addEventListener('click', () => {
+        currentControl.value = item
+        currentControl.flag = false
+        currentControl.position.top = 0
+        currentControl.position.left = 0
+    })
+    divs.push(div)
+}
+
+function show() {
+    document.getElementById('setting').hidden = true
+    document.getElementById('bottom').hidden = false
+}
+
+function hiddenBottom() {
+    document.getElementById('setting').hidden = false
+    document.getElementById('bottom').hidden = true
+}
+
+function move(direction) {
+    const move_t = document.getElementById(currentControl.value)
+    const is_inline = currentControl.value.includes('inline')
+    if (move_t) {
+        switch (direction) {
+            case 'up':
+                if (is_inline) {
+                    move_t.style.top = `${currentControl.position.top -= 10}px`
+                } else {
+                    move_t.style.marginTop = `${currentControl.position.top -= 10}px`
+                }
+                break
+            case 'left':
+                if (is_inline) {
+                    move_t.style.left = `${currentControl.position.left -= 10}px`
+                } else {
+                    move_t.style.marginLeft = `${currentControl.position.left -= 10}px`
+                }
+                break
+            case 'right':
+                if (is_inline) {
+                    move_t.style.left = `${currentControl.position.left += 10}px`
+                } else {
+                    move_t.style.marginLeft = `${currentControl.position.left += 10}px`
+                }
+                break
+            case 'down':
+                if (is_inline) {
+                    move_t.style.top = `${currentControl.position.top += 10}px`
+
+                } else {
+                    move_t.style.marginTop = `${currentControl.position.top += 10}px`
+                }
+                break
+        }
+    }
+
+}
+
+function resetIt() {
+    div_boxes.forEach(item => {
+        const move_t = document.getElementById(item)
+        const is_inline = item.includes('inline')
+        if (move_t) {
+            if (!is_inline) {
+                move_t.style.marginTop = ''
+                move_t.style.marginLeft = ''
+            } else {
+                move_t.style.top = ''
+                move_t.style.left = ''
+            }
+
+        }
+    })
+    currentControl.value = '无(点击div选择目标)'
+    currentControl.position.top = 0
+    currentControl.position.left = 0
+
+}
+
+function init() {
+    const root = document.getElementById('root')
+    let moveSelectNode = document.getElementById('action')
+    let orders = document.getElementById('order')
+
+
+    let nodes = []
+    let divs = []
+    let buttons = []
+
+    for (let i = 1; i <= div_boxes.length; i++) {
+        let item = div_boxes[i - 1]
+        generateDiv(item, i, divs)
+        // init checkbox
+        let sp_t = document.getElementById(item + 'sp')
+        if (sp_t) {
+            sp_t.remove()
+        }
+        let sp = document.createElement('span')
+        sp.id = item + 'sp'
+
+        let checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        checkbox.checked = true
+        checkbox.value = item
+        checkbox.addEventListener('click', () => {
+            const target = document.getElementById(item)
+            if (!checkbox.checked && target) {
+                target.style.display = 'none'
+            } else {
+                target.style.display = ''
+            }
+        })
+
+        let span = document.createElement('span')
+        span.innerText = `#${i}-${item}`
+
+        //init order button
+        const button_t = document.getElementById(`${item}_btn`)
+        if (button_t) {
+            button_t.remove()
+        }
+        const button = document.createElement('button')
+        button.className = `${item.replace(/([A-Z])/g, "-$1").toLowerCase()}-color order-btn`
+        button.innerText = `#${i}-${item}`
+        button.draggable = true
+
+        button.id = `${item}_btn`
+        button.ondrop = (event) => {
+            event.preventDefault();
+            let data = event.dataTransfer.getData("button")
+            console.log(data, 'data');
+            let target_button = document.getElementById(data)
+            let src_button = event.target
+            console.log(src_button)
+            swap(target_button, src_button)
+        }
+        button.ondragover = (event) => {
+            event.preventDefault();
+        }
+        button.ondragstart = (event) => {
+            event.dataTransfer.setData("button", event.target.id);
+        }
+        buttons.push(button)
+
+
+        sp.append(checkbox, span)
+        nodes.push(sp)
+    }
+    root.append(...divs)
+    moveSelectNode.append(...nodes)
+    orders.append(...buttons)
+    setIndexPosition()
+}
+
+function swap(a, b) {
+    let temp = {
+        id: a.id,
+        innerText: a.innerText,
+        className: a.className
+    };
+    a.id = b.id
+    a.innerText = b.innerText
+    a.className = b.className
+    Object.assign(b, temp)
+}
+
+function setIndexPosition() {
+    const val = document.getElementById('select').value;
+    let zIndex = document.getElementsByClassName('z-index')
+    for (let item of zIndex) {
+        item.style.position = val
+    }
+}
+
+function change() {
+    const buttons = document.getElementById('order').querySelectorAll(`button[id$='_btn']`)
+    div_boxes = []
+    buttons.forEach(button => {
+        div_boxes.push(button.id.replace(/_btn/g, ''))
+    })
+    init()
+}
+init()
